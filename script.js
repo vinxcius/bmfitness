669a0de3da071d4179e87da7786a52d7badd0f33
Vinxcius <vinick08@hotmail.com>
2021-05-04 17:07:38 -0300

    Script Js Final

--- script.js
+++ script.js
@@ -28,36 +28,58 @@ function navSlide() {
 }
 navSlide()
 
-//Smooth Scroll em links
-const menuItems = document.querySelectorAll('.wrap a');
 
-const scrollToIdOnClick = (event) =>{
-  event.preventDefault();
-  const element = event.target;
-  const id = element.getAttribute('href');
-  const to = document.querySelector(id).offsetTop;
+const menuItems = document.querySelectorAll(
+  '.wrap a')
 
-  //offsetTop informa a distancia da section emm relação ao topo
-  console.log(to.offsetTop);
+menuItems.forEach(link => {
+  link.addEventListener('click', scrollToId)
+})
 
-  //primeiro atributo de scroll é o eixo X, segundo eixo Y
-  window.scroll({
-    top: to - 50,
-  }
-  )
+//Pega a altura e o código do ID clicado
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href') //pega o valor de href, ex: #contact
+
+  return document.querySelector(id).offsetTop
+  //pega o código do id que foi clicado
+  //OffsetTop informa a distância (em pixels) do topo para a sessão que foi clicada
 }
 
-menuItems.forEach(link =>{
-  link.addEventListener('click', scrollToIdOnClick)
-})
+//Scrolla para a sessão
+function scrollToId(event) {
+  //impede de aparecer as '#' de cada link (meusite.com/index.html#Portfolio)
+  event.preventDefault()
+
+  //-80 define a distancia entre a sessão e o navbar
+  const goToSection = getScrollTopByHref(event.target) - 50
+
+  scrollToPosition(goToSection)
+}
+
+
+//recebe a distancia entre o ID e a navbar e seta o scroll para smooth
+function scrollToPosition(goToSection) {
+
+  //scroll recebe dois parâmetros: valor de x e y;
+  //É possível passar um objeto também
+  //CODIGO NAO FUNCIONA EM TODOS OS BROWSERS
+  // window.scroll({
+  //     top: goToSection,
+  //     behavior: "smooth" //
+  // })
+
+  //Eixo X, Eixo Y e Duração
+  smoothScrollTo(0, goToSection)
+}
+
 
 // Caso deseje suporte a browsers antigos / que não suportam scroll smooth nativo
 /**
- * Smooth scroll animation
- * @param {int} endX: destination x coordinate
- * @param {int) endY: destination y coordinate
- * @param {int} duration: animation duration in ms
- */
+* Smooth scroll animation
+* @param {int} endX: destination x coordinate
+* @param {int) endY: destination y coordinate
+  * @param {int} duration: animation duration in ms
+  */
 function smoothScrollTo(endX, endY, duration) {
   const startX = window.scrollX || window.pageXOffset;
   const startY = window.scrollY || window.pageYOffset;
@@ -69,17 +91,17 @@ function smoothScrollTo(endX, endY, duration) {
 
   // Easing function
   const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+      if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+      return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
   };
 
   const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
-    if (time >= duration) {
-      clearInterval(timer);
-    }
-    window.scroll(newX, newY);
+      const time = new Date().getTime() - startTime;
+      const newX = easeInOutQuart(time, startX, distanceX, duration);
+      const newY = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+          clearInterval(timer);
+      }
+      window.scroll(newX, newY);
   }, 1000 / 60); // 60 fps
 };
